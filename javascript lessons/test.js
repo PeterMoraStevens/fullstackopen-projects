b5c26855305053783018463b7be3c570220e7078
Peter <petermorastevens@gmail.com>
2023-08-17 16:30:27 -0700

    learnt more about javascript

---
+++ javascript lessons/test.js
@@ -0,0 +1,26 @@
+const object1 = {
+    name: 'Arto Hellas',
+    age: 35,
+    education: 'PhD',
+  }
+  
+  const object2 = {
+    name: 'Full Stack web application development',
+    level: 'intermediate studies',
+    size: 5,
+  }
+  
+  const object3 = {
+    name: {
+      first: 'Dan',
+      last: 'Abramov',
+    },
+    grades: [2, 3, 5, 3],
+    department: 'Stanford University',
+  }
+
+object1.address = 'Oregon'
+object1['password'] = 4321
+
+console.log(object1.password)
+console.log(object1.address)

2f81774d7bd894284ec2031ad2fb24c323c29bb9
Peter <petermorastevens@gmail.com>
2023-09-11 20:30:38 -0700

    added the delete method

--- node projects/index.js
+++ node projects/index.js
@@ -20,11 +20,29 @@ let notes = [
 ]
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
+    res.send(`<h1>Hello World</h1>`)
 })
 
 app.get('/api/notes', (req, res) => {
     res.json(notes)
+})
+
+// :id using express allows us to dynamically recieve an id when looking at our requests from the internet
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).send(`connot find note ${req.params.id}`)
+  }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  res.status(204).end()
 })
 
 const PORT = 3001
